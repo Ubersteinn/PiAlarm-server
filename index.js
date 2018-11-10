94a19207d186ef661490e56240a49804fc7372d9
Nick Rubenstein <nrubenstein24@gmail.com>
2018-11-10 15:40:21 -0600

    Test omxplayer

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 "use strict";
 
-const connect = require("connect");
+const execFile = require('child_process').execFile;
 const path = require("path");
+const connect = require("connect");
 const serveStatic = require("serve-static");
 const serveStaticFile = require("connect-static-file");
 const compression = require("compression");
 const app = connect();
+
+const omxplayer_process;
+const playaudio = (file) => {
+  if(omxplayer_process) {
+    omxplayer_process.kill('9');
+    omxplayer_process = null;
+  }
+  omxplayer_process = execFile('omxplayer', ['-o','local',file], (error, stdout, stderr) => {
+    if (error) {
+        console.error('stderr', stderr);
+        throw error;
+    }
+    console.log('stdout', stdout);
+    omxplayer_process = null;
+  });
+}
+
+setInterval(() => {
+  playaudio('audio/example.mp3');
+}, 10000);
 
 const PORT = 9000;
 const DIRECTORY = "public";
